195f1afd80f209d55748f3ea2f8a3b2a9886f7fd
DanielL29 <danielnatsufire@gmail.com>
2023-01-28 23:35:37 -0300

    feat: creating html configs to next and joining header and footer to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <h1>mks sistemas</h1>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
