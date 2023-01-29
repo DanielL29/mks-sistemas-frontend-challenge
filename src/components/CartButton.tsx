7b1499944a7d0d11744a60994f2be3cccef64bf5
DanielL29 <danielnatsufire@gmail.com>
2023-01-28 23:34:24 -0300

    feat: header of application and set cart open navbar

---
+++ src/components/CartButton.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import styled from "styled-components";
+import cartIcon from "../assets/images/cart.svg";
+import { CartStyleProps } from "./Header";
+
+interface CartButtonProps {
+  setCart: (cart: CartStyleProps) => void;
+}
+
+export default function CartButton({ setCart }: CartButtonProps) {
+  return (
+    <CartButtonWrapper
+      onClick={() =>
+        setCart({ cartWidth: "486px", invisibleScreenWidth: "100%" })
+      }
+    >
+      <Image src={cartIcon} alt="cart" width={30} height={30} />
+      <span>0</span>
+    </CartButtonWrapper>
+  );
+}
+
+const CartButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+
+  width: 90px;
+  height: 45px;
+  border-radius: 8px;
+  background-color: #fff;
+  cursor: pointer;
+
+  img {
+    height: auto;
+    width: auto;
+    height: 18px;
+    width: 19px;
+  }
+
+  span {
+    font-weight: 700;
+    font-size: 18px;
+    color: #000;
+  }
+`;
---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import Cart from "./Cart";
+import { useState } from "react";
+import CartButton from "./CartButton";
+import Image from "next/image";
+import cartIcon from "../assets/images/cart.svg";
+
+export interface CartStyleProps {
+  cartWidth: string;
+  invisibleScreenWidth: string;
+}
+
+export default function Header() {
+  const [cart, setCart] = useState<CartStyleProps>({
+    cartWidth: "0px",
+    invisibleScreenWidth: "0%",
+  });
+
+  return (
+    <HeaderWrapper>
+      <Logo>
+        <h1>MKS</h1>
+        <h2>Sistemas</h2>
+      </Logo>
+      <Cart cart={cart} setCart={setCart} />
+      <CartButton setCart={setCart} />
+    </HeaderWrapper>
+  );
+}
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 101px;
+  left: 0;
+  top: 0;
+  background: #0f52ba;
+  position: fixed;
+  padding: 0 70px;
+  z-index: 1;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  color: #fff;
+  gap: 10px;
+
+  h1 {
+    font-weight: 600;
+    font-size: 40px;
+  }
+
+  h2 {
+    font-weight: 300;
+    font-size: 20px;
+  }
+`;
