25531e02fd750bc95d72389a1ea0b60944802d1a
DanielL29 <danielnatsufire@gmail.com>
2023-01-30 01:40:43 -0300

    feat: list products in screen from api

--- src/layouts/Products/index.tsx
+++ src/layouts/Products/index.tsx
@@ -1,18 +1,36 @@
-import Product from "@/components/Product";
+import ProductCard from "@/components/ProductCard";
+import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
+import { listProducts } from "@/store/features/listProducts";
 import styled from "styled-components";
+import { useEffect } from "react";
 
 export default function Products() {
+  const dispatch = useAppDispatch();
+  const { products, loading } = useAppSelector((state) => state.listProducts);
+
+  useEffect(() => {
+    (async () => {
+      await dispatch(listProducts());
+    })();
+
+    return () => {};
+  }, []);
+
   return (
     <ProductsWrapper>
       <div>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {products.map((product: any) => {
+          return (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              description={product.description}
+              photo={product.photo}
+              price={product.price}
+              isLoading={loading}
+            />
+          );
+        })}
       </div>
     </ProductsWrapper>
   );
